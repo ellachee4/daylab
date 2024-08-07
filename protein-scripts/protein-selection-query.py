c964d4ac6e9aaea35455821d7c80e30dc29b1991
ellachee4 <ellachee4@gmail.com>
2024-08-07 12:18:27 -0400

    Final fixes and scores for protein query

--- protein-scripts/protein-selection-query.py
+++ protein-scripts/protein-selection-query.py
@@ -7,12 +7,13 @@
 # antibody availability, and final score for protein selection based on factors (weighted by importance).
 
 # Imports
-import sys
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from Bio import Entrez
 import sys
 import csv
@@ -128,10 +129,27 @@ def go_score(go):
     else:
         return 0
 
+
+#----------------- UV Score -----------------#
+def map_uv(timepoint):
+    '''Assign timepoints to UV scores. '''
+    if timepoint=='mock':
+        return -4
+    elif timepoint=='1h':
+        return 1
+    elif timepoint=='24h':
+        return 2
+    else:
+        return 3
+
+def uv_score(uv):
+    ''' Calculate UV score based on the timepoints of UV treatment.'''
+    uv_list = list(set(uv.split(':')))
+    print('UV scores calculated')
+    return sum(map(map_uv, uv_list))
+
 def query_go_terms(uniprots):
-    '''
-    Query GO terms for a list of proteins and return a list of GO scores and terms.
-    '''
+    '''Query GO terms for a list of proteins and return a list of GO scores and terms.'''
 
     scores = []
     terms = []
@@ -149,24 +167,6 @@ def query_go_terms(uniprots):
     print('Queried GO terms')
     return scores, terms
 
-
-#----------------- UV Score -----------------#
-def map_uv(timepoint):
-    if timepoint=='mock':
-        return -4
-    elif timepoint=='1h':
-        return 1
-    elif timepoint=='24h':
-        return 2
-    else:
-        return 3
-
-def uv_score(uv):
-    uv_list = list(set(uv.split(':')))
-    print('UV scores calculated')
-    return sum(map(map_uv, uv_list))
-
-
 #----------------- Antibody Availability -----------------#
 def scrape_antibodypedia_data(uniprot_id):
     '''
@@ -175,16 +175,19 @@ def scrape_antibodypedia_data(uniprot_id):
     '''
 
     # Set up the Selenium  WebDriver, construct URL, navigate to URL
-    driver = webdriver.Chrome()
-    base_url = 'https://www.antibodypedia.com/explore/uniprot%3A'
-    url = f'{base_url}{uniprot_id}'
-    driver.get(url)
+    options = Options()
+    options.add_argument('--start-maximized')
+    driver = webdriver.Chrome(options=options)
+    url = f'https://www.antibodypedia.com/explore/uniprot%3A{uniprot_id}'
+    try:
+        driver.get(url)
+    except Exception as error:
+        print(f"Error for {uniprot_id}: {error}")
 
     # Load table
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "search_results_table"))
     )
-    #print('Found table for UNIPROT:', uniprot_id)
 
     # Extract the link to the antibodies and number of antibodies
     try:
@@ -193,9 +196,6 @@ def scrape_antibodypedia_data(uniprot_id):
             antibodies_link = link_tag.get_attribute('href')
             antibody_id = str(link_tag.get_attribute('href'))[35:]
             number_of_antibodies = link_tag.text.split(' ')[0]
-            #print('Antibodies Link:', antibodies_link)
-            #print('Number of Antibodies:', number_of_antibodies)
-            #print('Antibody ID:', antibody_id)
     except Exception as error:
         print('Error: no antibodies found for UNIPROT:', uniprot_id)
         antibodies_link = 'None'
@@ -208,12 +208,9 @@ def scrape_antibodypedia_data(uniprot_id):
         if providers_div:
             try:
                 number_of_providers = providers_div.find_element(By.XPATH, '//*[@id="search_results_table"]/tbody/tr/td[6]/div/b').text
-                #print('Number of Providers:', number_of_providers)
             except Exception as error:
-                #print('Error: no providers found for UNIPROT:', uniprot_id)
                 number_of_providers = 0
     except Exception as error:
-        #print('Error: no providers found for UNIPROT:', uniprot_id)
         number_of_providers = 0
 
     driver.quit()
@@ -235,16 +232,14 @@ def track_references_antipodypedia(antibody_id):
     
     # Load table
     WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "featured_antibodies"))
+        EC.presence_of_element_located((By.ID, "content"))
     )
-    #print('Found table for antibody:', antibody_id)
 
     # Extract the link to the antibodies and number of antibodies
-    try:
+    try: 
         link_tag = driver.find_element(By.XPATH, '//*[@id="filter_results"]/b[1]')
         if link_tag:
             referenced_antibodies = link_tag.text
-            #print('Number of Referenced Antibodies:', referenced_antibodies)
     except Exception as error:
         print('Error: no references found for antibody:', antibody_id)
         referenced_antibodies = 0
@@ -286,7 +281,7 @@ def compute_score(row):
     # present at 3 timepoints, mock is one of them (fix uv score)
     # any term present gets increase score if found in abstract
 
-    score = row['Article Count (normalized)']*-2 + row['Number of Antibodies (normalized)']*0.5\
+    score = row['Article Count (normalized)']*-2 + row['Number of ref-ed Antibodies (normalized)']*0.5\
             + row['Interactions (normalized)'] + row['GO Score (normalized)'] + row['UV Score (normalized)']
     return score
 
@@ -311,13 +306,13 @@ def main(input_csv, output_csv):
     print('Read input CSV proteins')
 
     # Sorted list of proteins and their associated article counts
+    links, num_antibodies, num_providers, num_referenced = query_antibodypedia(uniprots)
     article_counts = query_pubmed(symbols)
     mapping = uniprot_to_string(uniprots)
     print('Not converted:', [u for u in uniprots if u not in mapping.keys()])
     strings = [mapping[uni] if uni in mapping.keys() else '' for uni in uniprots]
     interactions = query_string(strings)
     go_scores, go_terms = query_go_terms(uniprots)
-    links, num_antibodies, num_providers, num_referenced = query_antibodypedia(uniprots)
 
     data_dict = {
         'Uniprot': uniprots,
@@ -335,7 +330,7 @@ def main(input_csv, output_csv):
 
     df_all = pd.DataFrame(data_dict)
     df_all['UV Score'] = df_all['UV_treatment'].apply(lambda x: uv_score(x))
-    for col in ['Number of Antibodies', 'Article Count', 'Interactions', 'GO Score', 'UV Score']:
+    for col in ['Number of Ref-ed Antibodies', 'Article Count', 'Interactions', 'GO Score', 'UV Score']:
         df_all[col] =pd.to_numeric(df_all[col])
         df_all[f'{col} (normalized)'] = df_all[col]/df_all[col].std()
 
